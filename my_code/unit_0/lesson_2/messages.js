72ee2c429f984bb518de7c8a513b79ed27730d29
cadee247 <rousseaucadee@gmail.com>
2025-04-15 14:28:02 +0200

    Add files via upload

---
+++ my_code/unit_0/lesson_2/messages.js
@@ -0,0 +1,5 @@
+let randomArray = ["Cadee, 19, true"];
+
+randomArray.forEach(anything => {
+    console.log
+})
---
+++ my_code/unit_0/lesson_2/quickCheck.js
@@ -0,0 +1,9 @@
+// Quick check 2.1 If you have a file called hello.js, what will happen if you run node hello in
+//  terminal?
+
+// it will break the session, because the correct command is Node hello.js
+
+// Quick check 2.2 
+// 1 What are three ways in which you could exit the REPL environment?
+//  2 How do you load a file that isn’t in your project folder into REPL?
+//  3 What happens if you run .save with a filename that already exists?
